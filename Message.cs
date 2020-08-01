755d196a1b9f47c07bb8a06b35b7f59e7e77668f
Utharn Buranasaksee <utarn@yahoo.com>
2020-08-01 10:20:04 +0700

    Starter Project

---
+++ Message.cs
@@ -0,0 +1,12 @@
+namespace pattern_facade
+{
+    public class Message
+    {
+        private  string _content;
+
+        public Message(string content)
+        {
+            _content = content;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pattern_facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
